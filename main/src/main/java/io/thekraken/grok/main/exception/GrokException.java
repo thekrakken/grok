20429c089a614f8963b5dc91668509ddfa436f53
anthony corbacho <corbacho.anthony@gmail.com>
2014-04-14 12:14:12 +0900

    rename configuration

---
+++ main/src/main/java/io/thekraken/grok/main/exception/GrokException.java
@@ -0,0 +1,49 @@
+package io.thekraken.grok.main.exception;
+
+/**
+ * Signals that an {@code Grok} exception of some sort has occurred. This class is the general class
+ * of exceptions produced by failed or interrupted Grok operations.
+ *
+ * @author anthonycorbacho
+ * @since 0.0.1
+ */
+public class GrokException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Creates a new GrokException.
+   */
+  public GrokException() {
+    super();
+  }
+
+  /**
+   * Constructs a new GrokException.
+   *
+   * @param message the reason for the exception
+   * @param cause the underlying Throwable that caused this exception to be thrown.
+   */
+  public GrokException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  /**
+   * Constructs a new GrokException.
+   *
+   * @param message the reason for the exception
+   */
+  public GrokException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new GrokException.
+   *
+   * @param cause the underlying Throwable that caused this exception to be thrown.
+   */
+  public GrokException(Throwable cause) {
+    super(cause);
+  }
+
+}
